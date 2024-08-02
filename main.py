e7eabee605f0ebc704a8417ab382740d276cf2bd
Rahul <rahulmathur87@gmail.com>
2024-08-03 04:59:38 +0530

    add blog json

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+blog_endpoint = 'https://api.npoint.io/982b6858478d3009210b'
+response = requests.get(blog_endpoint)
+response.raise_for_status()
+blogs = response.json()
 
 
 @app.route('/')
